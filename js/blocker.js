c54d6fa4522ff3d87e530ce2a6c641a422105d83
FrancescoSTL <francescosapolizzi@gmail.com>
2017-03-02 21:03:30 -0600

    Adds blocking example (commented)

--- js/blocker.js
+++ js/blocker.js
@@ -1,3 +1,13 @@
-chrome.webRequest.onSendHeaders.addListener(function(details) {
-    console.log(details.url);
-}, {urls:["*://*/*"]}, ["requestHeaders"]);
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+    
+    var areWeCancelling;
+    /*if(details.url.indexOf("://www.google.com") != -1) {
+    	areWeCancelling = true;
+    	console.log("bitch we be blockin " + details.url);
+    } else {*/
+    	console.log(details.url);
+    //}
+   
+
+    return {cancel: areWeCancelling};
+},{urls:["*://*/*"]}, ["blocking", "requestHeaders"]);
