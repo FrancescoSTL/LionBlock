2bd62b1285b2c6ecc94febe9f54c50b014db1175
Madeluccar <madeluccar@hotmail.com>
2017-03-24 14:20:07 -0500

    Added front end. Issues #13, #12, #10 and partially completed #9 and #11

--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
-document.write("sup");
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// document.write("sup"); 
+function showOptions() {
+  document.getElementById('options').style.visibility='hidden';
+}
+
+$("#click-tag").on('click', function(event){
+    //document.getElementById('options').style.visibility='visible';
+    $('.option-container').toggleClass('open');
+});
